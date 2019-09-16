caf7feca7f0327d0c6ecadc02cc31595223ce6cd
AhmedAlaa20140031 <ahmedalaahussein00@gmail.com>
2019-09-16 19:20:35 +0200

    Learn Input widgets

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,42 @@ class MyApp extends StatefulWidget {
 }
 
 class _State extends State<MyApp> {
+
+  int _value1 = 0;
+  int _value2 = 0;
+
+  bool _switch1 = false;
+  bool _switch2 = false;
+
+  void _setValue1 (int value) => setState(()=> _value1 = value);
+  void _setValue2 (int value) => setState(()=> _value2 = value);
+
+  void _onChangedSwitch1(bool value)setState(()=> _switch1 = value);
+  void _onChangedSwitch2(bool value)setState(()=> _switch2 = value);
+
+
+  Widget makeRadios()
+  {
+    List list = new List();
+    for(int i = 0;i < 3;i++)
+    {
+      list.add(new Radio(value:i,groupValue:_value1,onChanged:_setValue1));
+    }
+    Column column = new Column(children:list);
+    return column;
+  }  
+
+  Widget makeRadiosTiles()
+  {
+    List list = new List();
+    for(int i = 0;i < 3;i++)
+    {
+      list.add(new RadiosListTiles(value:i,groupValue:_value2,onChanged:_setValue2));
+    }
+    Column column = new Column(children:list);
+    return column;
+  }  
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -22,7 +58,16 @@ class _State extends State<MyApp> {
         padding: new EdgeInsets.all(32.0),
         child: new Column(
           children: <Widget>[
-            new Text('Add Widgets Here')
+            new Text('Add Widgets Here'),
+            makeRadiosTiles(),
+            makeRadios(),
+            new Switch (value: _switch1,onChanged: _onChangedSwitch1),
+            new SwitchListTile (
+              value:_switch2,
+              onChanged:_onChangedSwitch2,
+              title: new Text("Switch one"),
+              subtitle: new Text("Subtitle")
+            )
           ],
         ),
       ),
